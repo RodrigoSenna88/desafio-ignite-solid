c8ed2131f2264e735b330d30d990f7795e09b792
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-04-26 18:41:44 -0300

    testes de userRepositry, createUser e TurnUserAdmin passando

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -24,8 +24,9 @@ class UsersRepository implements IUsersRepository {
     Object.assign(user, {
       name,
       email,
-      created_at: Date(),
-      updated_at: Date(),
+      admin: false,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.users.push(user);
