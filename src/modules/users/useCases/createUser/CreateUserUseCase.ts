e49caa82060632953c8422d9dd82d3a45a5dc061
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2021-04-26 17:48:16 -0300

    fazendo controller e useCase de CreateUser

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -10,7 +12,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const userCreated = this.usersRepository.create({ email, name });
+
+    return userCreated;
   }
 }
 
